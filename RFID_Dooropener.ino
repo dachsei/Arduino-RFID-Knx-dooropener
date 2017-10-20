21c3d97e97ae19b1cd44d2221bed9525a9c7a90c
Philip Dachs <philip.dachs@web.de>
2017-10-20 15:17:25 +0200

    Changed Reading workflow

--- RFID_Dooropener.ino
+++ RFID_Dooropener.ino
@@ -4,43 +4,44 @@
 #include "keys.h"
 
 #define SS_PIN 10
-#define RST_PIN 9
-#define ID_GA "0/7/0"
-#define PROGRAM_GA "0/7/1"
-#define DEBUG_GA "0/7/2"    //GA to write Debug Text to
+#define RST_PIN A1
+#define SYNC_GA "0/7/255"       //Shared by all, disables all other readers if tag detected
+#define WRITE_OK_GA "0/7/254"   //Shared by all, signales succesful id write
+#define NEWID_GA "0/7/253"      //Shared by all, new id to place on tag
+#define READTAG_GA "0/7/2"      //Must be unique for every different acces controll
+#define PHYSICAL_ADDR "15.15.2"
 
-long programmTime = 5000;
+void (* volatile stateFunc)() = &readNewTag;
+uint16_t stateChangeMillis = 0;
+char newId[16];
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-KnxTpUart knx(&Serial, "15.15.19");
-
-
-volatile bool programm = false;
-byte newId[16];
+KnxTpUart knx(&Serial, PHYSICAL_ADDR);
 
 void setup()
 {
   SPI.begin();
   mfrc522.PCD_Init();
+  //Serial.begin(9600);
   
-  Serial.begin(19200);
-  UCSR0C = UCSR0C | B00100000; // Even Parity
+  Serial.begin(19200, SERIAL_8E1);
   knx.uartReset();
-  knx.addListenGroupAddress(PROGRAM_GA);
+  knx.addListenGroupAddress(SYNC_GA);
+  knx.addListenGroupAddress(NEWID_GA);
 }
+int sent = 0;
 
 void loop()
 {
-  MFRC522::StatusCode result;
-
-  if(programm) {
-    if(!programmId()) {
-      #ifdef DEBUG_GA
-      knx.groupWrite14ByteText(DEBUG_GA, "Timeout");
-      #endif
-    }
-    programm = false;
+  if(millis() > 1000 && sent == 0) {
+    knx.groupWrite14ByteText("0/7/99", "asdf");
+    sent = 1;
   }
-  
+  stateFunc();
+}
+
+void readNewTag()
+{
   if ( !mfrc522.PICC_IsNewCardPresent()) return;
   if ( !mfrc522.PICC_ReadCardSerial()) return;
 
@@ -50,73 +51,71 @@ void loop()
     delay(1000);
     return;
   }
+  knx.groupWriteBool(SYNC_GA, true);
+  //Serial.println("Tag detected, other readers disabled");
+  delay(5);
+  stateFunc = &readSyncronized;
+}
 
-  byte buffer[18];
+void readSyncronized()
+{
+  char buffer[18];
   byte size = sizeof(buffer);
   if(mfrc522.MIFARE_Read(6, buffer, &size) == MFRC522::STATUS_OK) {
     buffer[16] = '\0';
-    knx.groupWrite14ByteText(ID_GA, (const char*)buffer);
+    knx.groupWrite14ByteText(READTAG_GA, buffer);
+    //Serial.print("Tag read: ");
+    //Serial.println(buffer);
+    stateChangeMillis = millis();
+    stateFunc = &waitForNewId;
   }
-
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  else {
+    knx.groupWriteBool(SYNC_GA, false);
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
+    stateFunc = &readNewTag;
+  }
 }
 
-bool programmId()
+void waitForNewId()
 {
-  long startTime = millis();
-  byte tryKey = 0;
-  do {
-    if ( !mfrc522.PICC_IsNewCardPresent()) continue;
-    if ( !mfrc522.PICC_ReadCardSerial()) continue;
-
-    if(tryKey == 0) {
-      if(mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 7, &key, &mfrc522.uid) != MFRC522::STATUS_OK) {
-        tryKey = 1;
-        continue;
-      }
-    }
-    else {
-      if(mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 7, &keyDefault, &mfrc522.uid) != MFRC522::STATUS_OK) {
-        tryKey = 0;
-        continue;
-      }
-      else {  //Unprogrammed Card
-        byte buffer[18];
-        byte size = sizeof(buffer);
-        if(mfrc522.MIFARE_Read(7, buffer, &size) == MFRC522::STATUS_OK) {
-          memcpy(buffer, key.keyByte, 6);
-          if(mfrc522.MIFARE_Write(7, buffer, 16) == MFRC522::STATUS_OK) {
-            #ifdef DEBUG_GA
-            knx.groupWrite14ByteText(DEBUG_GA, "Neuer Tag");
-            #endif
-            /*Serial.println("Fresh Card, programmed Key_A");
-            printArray(buffer, 16);*/
-          }
-        }
-      }
-    }
-    //Authenticated Card
-    if(mfrc522.MIFARE_Write(6, newId, 16) == MFRC522::STATUS_OK) {
-      #ifdef DEBUG_GA
-      knx.groupWrite14ByteText(DEBUG_GA, "Program. OK");
-      #endif
-      mfrc522.PICC_HaltA();
-      mfrc522.PCD_StopCrypto1();
-      return true;
-    }
+  if(uint16_t(millis() - stateChangeMillis) >= 3000) {
+    knx.groupWriteBool(SYNC_GA, false);
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
+    stateFunc = &readNewTag;
   }
-  while((millis() - startTime) < programmTime);
-  return false;
+}
+
+void programNewId()
+{
+  if(mfrc522.MIFARE_Write(6, newId, 16) == MFRC522::STATUS_OK) {
+    knx.groupWriteBool(WRITE_OK_GA, true);
+    //Serial.print("Write erfolgreich: ");
+    //Serial.println(newId);
+  }
+  else {
+    knx.groupWriteBool(WRITE_OK_GA, false);
+    //Serial.println("Write fehlgeschlagen");
+  }
+  delay(50);
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+  stateFunc = &readNewTag;
+  knx.groupWriteBool(SYNC_GA, false);
+}
+
+void syncWait()
+{
+  if(uint16_t(millis() - stateChangeMillis) >= 5000) {
+    stateFunc = &readNewTag;
+  }
 }
 
 void serialEvent()
 {
   KnxTpUartSerialEventType eType = knx.serialEvent();
-  if (eType == TPUART_RESET_INDICATION) {
-    
-  }
-  else if (eType == KNX_TELEGRAM) {
+  if (eType == KNX_TELEGRAM) {
     KnxTelegram* telegram = knx.getReceivedTelegram();
     String target =
       String(0 + telegram->getTargetMainGroup())   + "/" +
@@ -124,13 +123,33 @@ void serialEvent()
       String(0 + telegram->getTargetSubGroup());
 
     if (telegram->getCommand() == KNX_COMMAND_WRITE) {
-      if (target == PROGRAM_GA) {
-        memset(newId, 0, 16);
-        String knxId = telegram->get14ByteValue();
-        for(byte i = 0; i < knxId.length(); ++i) {
-          newId[i] = knxId[i];
+      if (target == NEWID_GA) {
+        if(stateFunc == &waitForNewId) {
+          memset(newId, 0, 16);
+          String knxId = telegram->get14ByteValue();
+          for(byte i = 0; i < knxId.length(); ++i) {
+            newId[i] = knxId[i];
+          }
+          stateFunc = &programNewId;
         }
-        programm = true;
+      }
+      else if(target == SYNC_GA) {
+        String source =
+          String(telegram->getSourceArea()) + "." +
+          String(telegram->getSourceLine()) + "." +
+          String(telegram->getSourceMember());
+        if(source != PHYSICAL_ADDR) {
+          if(telegram->getBool()) {
+            stateFunc = &syncWait;
+            //Serial.println("Disabled by sync");
+          }
+          else if(stateFunc == syncWait) {
+            stateFunc = &readNewTag;
+            mfrc522.PICC_HaltA();
+            mfrc522.PCD_StopCrypto1();
+            //Serial.println("Going back to work");
+          }
+        }
       }
     }
   }
